bc1687d72745cefe78cb34aa36987b7475a0d600
dingxwsimon <dingxwsimon@gmail.com>
2015-03-21 16:41:40 -0700

    modify devide number code

--- src/LeetCode/Devide.java
+++ src/LeetCode/Devide.java
@@ -5,54 +5,50 @@ public class Devide
 
   public static int divide1(int dividend, int divisor)
   {
-    int a = Math.abs(dividend);
-    int b = Math.abs(divisor);
+      if (divisor == 1) // Trival case 1
+          return dividend;
 
-    // b maybe Integer.MIN_VALUE
+      // Use negative integers to avoid integer overflow
+      if (dividend > 0)
+          return -divide(-dividend, divisor);
+      if (divisor > 0)
+          return -divide(dividend, -divisor);
 
-    boolean neg = (dividend > 0 && divisor < 0)
-        || (dividend < 0 && divisor > 0);
-    if (divisor == 0) {
+      if (dividend > divisor) // Trivial case 2
+          return 0;
 
-      return Integer.MAX_VALUE;
-    }
-    if (divisor == Integer.MIN_VALUE) {
-      if (dividend == Integer.MIN_VALUE) {
-        return 1;
-      }
-      else {
-        return 0;
-      }
-    }
-    if (dividend == Integer.MIN_VALUE) {
-      if (neg) {
-        return -1 + divide(dividend + b, b);
-      }
-      else {
-        return 1 - divide(dividend + b, b);
-      }
-    }
+      if ((dividend == Integer.MIN_VALUE) && (divisor == -1)) // Overflow case
+          return Integer.MAX_VALUE;
 
-    int product = b, result = 0;
-    while (a >= b) {
-      int q = 1;
-      while (a - product >= product) {
-        q = q << 1;
-        product = product << 1;
+      // Find the highest mult = (divisor * 2^shifts) which is <= dividend
+      // by shifting mult to the left without causing an overflow.
+      // At most (log2(|dividend|) - log2(|divisor|) + 1) iterations.
+      int min_divisor = Integer.MIN_VALUE >> 1;
+      int mult = divisor; // = divisor * 2^shifts
+      int shifts = 0;
+      while ((mult >= min_divisor) && (mult > dividend)) {
+          mult <<= 1;
+          ++shifts;
       }
-      System.out.println("a " + a + " b = " + b + " product = " + product
-          + " q = " + q);
-      a = a - product;
-      product = b;
-      result += q;
-    }
 
-    if (!neg) {
+      // Compute the result by shifting mult to the right.
+      // At most (log2(|dividend|) - log2(|divisor|) + 1) iterations for the outer loop.
+      // At most (log2(|dividend|) - log2(|divisor|) + 1) iterations for the inner loop
+      // (in total, not per outer iteration).
+      int result = 0;
+      int power = 1 << shifts; // = 2^shifts
+      while (dividend <= divisor) {
+          shifts = 0;
+          while (mult < dividend) {
+              mult >>= 1;
+              ++shifts;
+          }
+          dividend -= mult;
+          power >>= shifts;
+          result |= power; // Adds power to result
+      }
+
       return result;
-    }
-    else {
-      return -result;
-    }
   }
 
   // slow
