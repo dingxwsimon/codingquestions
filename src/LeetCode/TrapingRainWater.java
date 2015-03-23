997d556306cd10e28d8e330dd3f2743f1d92e465
dingxwsimon <dingxwsimon@gmail.com>
2015-03-23 09:11:04 -0700

    add idea comments

--- src/LeetCode/TrapingRainWater.java
+++ src/LeetCode/TrapingRainWater.java
@@ -2,6 +2,21 @@ package LeetCode;
 
 public class TrapingRainWater {
 
+    /*
+     * The idea is: calculated the stored water at each index a and b in my
+     * code. At the start of every loop, update the current maximum height
+     * from left side (that is from A[0,1...a]) and the maximum height from
+     * right side(from A[b,b+1...n-1]). if(leftmax<rightmax) then, at least
+     * (leftmax-A[a]) water can definitely be stored no matter what happens
+     * between [a,b] since we know there is a barrier at the
+     * rightside(rightmax>leftmax). On the other side, we cannot store more
+     * water than (leftmax-A[a]) at index a since the barrier at left is of
+     * height leftmax. So, we know the water that can be stored at index a is
+     * exactly (leftmax-A[a]). The same logic applies to the case when
+     * (leftmax>rightmax). At each loop we can make a and b one step closer.
+     * Thus we can finish it in linear time.
+     */
+
     public int trap1(int[] A) {
 	if (A == null || A.length == 0) {
 	    return 0;
