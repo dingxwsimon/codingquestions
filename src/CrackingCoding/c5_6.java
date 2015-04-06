e938b67d962f63d2147a2a869641974f31fa3248
Xiaowen Ding <dingxwsimon@gmail.com>
2015-04-05 20:51:00 -0700

    more questions

---
+++ src/CrackingCoding/c5_6.java
@@ -0,0 +1,14 @@
+package CrackingCoding;
+
+public class c5_6 {
+    public static int swapOddEvenBits(int x) {
+	return (((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1));
+    }
+
+    public static void main(String[] args) {
+	int a = 103217;
+	System.out.println(a + ": " + AssortedMethods.toFullBinaryString(a));
+	int b = swapOddEvenBits(a);
+	System.out.println(b + ": " + AssortedMethods.toFullBinaryString(b));
+    }
+}
