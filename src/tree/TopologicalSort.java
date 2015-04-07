6aea70ae9cea6d5a083313cae6ac1bab6b52fefb
Xiaowen Ding <dingxwsimon@gmail.com>
2015-04-06 20:00:39 -0700

    toplogy sorting

---
+++ src/tree/TopologicalSort.java
@@ -0,0 +1,58 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+
+
+//DFS style
+//http://en.wikipedia.org/wiki/Topological_sorting
+public class TopologicalSort {
+    public static class DirectedGraphNode {
+	int label;
+	ArrayList<DirectedGraphNode> neighbors;
+
+	DirectedGraphNode(int x) {
+	    label = x;
+	    neighbors = new ArrayList<DirectedGraphNode>();
+	}
+    };
+
+    public ArrayList<DirectedGraphNode> topSort(
+	    ArrayList<DirectedGraphNode> graph) {
+	// write your code here
+	ArrayList<DirectedGraphNode> res = new ArrayList<DirectedGraphNode>();
+	if (graph == null || graph.size() == 0) {
+	    return res;
+	}
+
+	Stack<DirectedGraphNode> st = new Stack<DirectedGraphNode>();
+	Set<DirectedGraphNode> visited = new HashSet<DirectedGraphNode>();
+	for (DirectedGraphNode node : graph) {
+	    if (!visited.contains(node)) {
+		visit(st, node, visited);
+	    }
+	}
+
+	while (!st.isEmpty()) {
+	    res.add(st.pop());
+	}
+
+	return res;
+    }
+
+    public void visit(Stack<DirectedGraphNode> st, DirectedGraphNode node,
+	    Set<DirectedGraphNode> visited) {
+	visited.add(node);
+
+	for (DirectedGraphNode neb : node.neighbors) {
+	    if (!visited.contains(neb)) {
+		visit(st, neb, visited);
+	    }
+	}
+
+	st.push(node);
+    }
+}
--- src/tree/TopologySort.java
+++ src/tree/TopologySort.java
@@ -1,12 +1,58 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+
+//http://en.wikipedia.org/wiki/Topological_sorting
 public class TopologySort {
+
+    public static class Node {
+	public final String name;
+	public final HashSet<Edge> inEdges;
+	public final HashSet<Edge> outEdges;
+
+	public Node(String name) {
+	    this.name = name;
+	    inEdges = new HashSet<Edge>();
+	    outEdges = new HashSet<Edge>();
+	}
+
+	public Node addEdge(Node node) {
+	    Edge e = new Edge(this, node);
+	    outEdges.add(e);
+	    node.inEdges.add(e);
+	    return this;
+	}
+
+	@Override
+	public String toString() {
+	    return name;
+	}
+    }
+
+    public static class Edge {
+	public final Node from;
+	public final Node to;
+
+	public Edge(Node from, Node to) {
+	    this.from = from;
+	    this.to = to;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+	    Edge e = (Edge) obj;
+	    return e.from == from && e.to == to;
+	}
+    }
+
     /*
      * 有n个任务需要完成（编号1到n），任务之间有一些依赖关系，如果任务a依赖于任务b和c，那么只有当任务b和任务c完成之后才能完成任务a。给定所有的依赖关系
      * ，判断这些任务是否能够完成。如果能够完成，请给出一个合法的任务完成序列。 样例： n=5 1->2,3 3->4
@@ -54,8 +100,68 @@ public class TopologySort {
     }
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	Node seven = new Node("7");
+	Node five = new Node("5");
+	Node three = new Node("3");
+	Node eleven = new Node("11");
+	Node eight = new Node("8");
+	Node two = new Node("2");
+	Node nine = new Node("9");
+	Node ten = new Node("10");
+	seven.addEdge(eleven).addEdge(eight);
+	five.addEdge(eleven);
+	three.addEdge(eight).addEdge(ten);
+	eleven.addEdge(two).addEdge(nine).addEdge(ten);
+	eight.addEdge(nine).addEdge(ten);
 
+	Node[] allNodes = { seven, five, three, eleven, eight, two, nine, ten };
+	// L <- Empty list that will contain the sorted elements
+	ArrayList<Node> L = new ArrayList<Node>();
+
+	// S <- Set of all nodes with no incoming edges
+	HashSet<Node> S = new HashSet<Node>();
+	for (Node n : allNodes) {
+	    if (n.inEdges.size() == 0) {
+		S.add(n);
+	    }
+	}
+	// while S is non-empty do
+	while (!S.isEmpty()) {
+	    // remove a node n from S
+	    Node n = S.iterator().next();
+	    S.remove(n);
+
+	    // insert n into L
+	    L.add(n);
+
+	    // for each node m with an edge e from n to m do
+	    for (Iterator<Edge> it = n.outEdges.iterator(); it.hasNext();) {
+		// remove edge e from the graph
+		Edge e = it.next();
+		Node m = e.to;
+		it.remove();// Remove edge from n
+		m.inEdges.remove(e);// Remove edge from m
+
+		// if m has no other incoming edges then insert m into S
+		if (m.inEdges.isEmpty()) {
+		    S.add(m);
+		}
+	    }
+	}
+	// Check to see if all edges are removed
+	boolean cycle = false;
+	for (Node n : allNodes) {
+	    if (!n.inEdges.isEmpty()) {
+		cycle = true;
+		break;
+	    }
+	}
+	if (cycle) {
+	    System.out.println("Cycle present, topological sort not possible");
+	} else {
+	    System.out.println("Topological Sort: "
+		    + Arrays.toString(L.toArray()));
+	}
     }
 
 }
